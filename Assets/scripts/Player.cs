c4d046775b76faadaaea4903cbe89b1efa54e1c4
hexfire17 <hexfire17@gmail.com>
2016-06-01 17:16:44 -0400

    cleanup

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -3,14 +3,12 @@ using System.Collections;
 
 public class Player : MonoBehaviour 
 {
-	// Use this for initialization
 	void Start () 
 	{
 		_camera = FindObjectOfType<Camera> ();
 		_isAiming = false;
 	}
 	
-	// Update is called once per frame
 	void Update ()
 	{
 		if (Input.GetMouseButtonDown (0)) 
--- Assets/scripts/Puck.cs
+++ Assets/scripts/Puck.cs
@@ -1,32 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Puck : MonoBehaviour {
-
-	// Use this for initialization
+public class Puck : MonoBehaviour 
+{
 	void Start ()
 	{
 		transform.localScale = Vector3.one * _size;
 		_rigidBody = GetComponent<Rigidbody2D> ();
 	}
 
-	// Update is called once per frame
-	void Update ()
-	{
-
-	}
-
 	public void launch (Vector2 direction)
 	{
 		Debug.Log ("Launching puck in direction: " + direction);
-		_launchDirection = direction;
-		_rigidBody.AddForce (_launchDirection.normalized * _speed);
+		_rigidBody.AddForce (direction.normalized * _speed);
 	}
 
 	public Transform _puckPrefab;
 	public float _size;
 	public float _speed;
 
-	Vector2 _launchDirection;
 	Rigidbody2D _rigidBody;
 }
