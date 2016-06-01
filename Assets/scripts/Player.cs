df9955a441bb991f7046950b731161f2114b01d6
hexfire17 <hexfire17@gmail.com>
2016-06-01 16:55:40 -0400

    setup git

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		_camera = FindObjectOfType<Camera> ();
+		_isAiming = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Vector3 spawnPosition = _camera.ScreenToWorldPoint (Input.mousePosition);
+			spawnPosition.z = 0;
+			_puck = Instantiate (_puckPrefab, spawnPosition, _puckPrefab.transform.rotation) as Puck; 
+			Debug.Log ("click");
+			_isAiming = true;
+		} 
+		else if (Input.GetMouseButtonUp (0))
+		{
+			Debug.Log ("click release");
+			_aimDirection = _puck.transform.position - _camera.ScreenToWorldPoint (Input.mousePosition);
+			_isAiming = false;
+			_puck.launch (_aimDirection);
+		}
+		else if (_isAiming) 
+		{
+			_aimDirection = _puck.transform.position - _camera.ScreenToWorldPoint (Input.mousePosition);
+		}
+	}
+
+	public Puck _puckPrefab;
+
+	Puck _puck;
+	Camera _camera;
+
+	bool _isAiming;
+	Vector2 _aimDirection;
+}
---
+++ Assets/scripts/Puck.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		transform.localScale = Vector3.one * _size;
+		_isLaunched = false;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (_isLaunched) 
+		{
+			transform.Translate (Time.deltaTime * _speed * _launchDirection);
+		}
+	}
+
+	public void launch (Vector2 direction)
+	{
+		Debug.Log ("Launching puck in direction: " + direction);
+		_launchDirection = direction;
+		_isLaunched = true;
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		Debug.Log ("Collide: " + col.contacts [0].point);
+//		float angle = Vector2.Angle(_launchDirection, );
+	//	Debug.Log ("Angle: " + angle);
+	}
+
+	public Transform _puckPrefab;
+	public float _size;
+	public float _speed;
+
+	bool _isLaunched;
+	Vector2 _launchDirection;
+}
